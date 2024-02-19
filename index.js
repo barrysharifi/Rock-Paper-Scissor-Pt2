fc0b52ce4b866eb3b463a72c8d80c1ef92e51ce4
Barrysharifi <barrysharifi@gmail.com>
2024-02-19 15:31:40 -0500

    added the playRound function which decides the winner of each individual round

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
 function getComputerChoice(){
     const choices = ['Rock', 'Paper', 'Scissors'] //list of choice
     const randomNumber = Math.floor(Math.random() * 3) //generates randome number 0 to 2
-    return choices[randomNumber] //chooses a choice based on the random number and the array position
+    let selection = choices[randomNumber].toLowerCase() //chooses a choice based on the random number and the array position
+    return selection
 }
-console.log(getComputerChoice())
+
+
+function playRound(playerSelection, computerSelection){
+    console.log('player picked ' + playerSelection)
+    console.log('computer picked ' + computerSelection)
+
+    playerSelection = playerSelection.toLowerCase();
+   
+    
+    if (playerSelection === computerSelection){
+        return `It's a tie!`
+    }
+    if(
+        (playerSelection === 'rock' && computerSelection === 'scissors') 
+        || (playerSelection === 'paper' && computerSelection === 'rock')
+        || (playerSelection === 'scissors' && computerSelection === 'paper')
+    ){
+            return "player wins!"
+        }
+        
+    if ((computerSelection === 'rock' && playerSelection === 'scissors')
+        ||(computerSelection === 'paper' && playerSelection === 'rock')
+        ||(computerSelection === 'scissors' && playerSelection === 'paper')
+    ){
+            return 'computer wins!'
+        }
+    
+}
+console.log(playRound('paper', getComputerChoice()))
+
+
+function playGame(){
+    let playerScore = ""
+    let computerScore = ""
+    let round = 0
+}
