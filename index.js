a7982c1b4f61407d16d29811eac8adea0b7e3275
Barrysharifi <barrysharifi@gmail.com>
2024-02-19 16:53:50 -0500

    completed console rock paper scissors for 5 rounds

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function getComputerChoice(){
+function getComputerChoice() {
     const choices = ['Rock', 'Paper', 'Scissors'] //list of choice
     const randomNumber = Math.floor(Math.random() * 3) //generates randome number 0 to 2
     let selection = choices[randomNumber].toLowerCase() //chooses a choice based on the random number and the array position
@@ -6,37 +6,42 @@ function getComputerChoice(){
 }
 
 
-function playRound(playerSelection, computerSelection){
+function playRound(playerSelection, computerSelection) {
     console.log('player picked ' + playerSelection)
     console.log('computer picked ' + computerSelection)
+    // playerSelection = playerSelection.toLowerCase();
 
-    playerSelection = playerSelection.toLowerCase();
-   
-    
-    if (playerSelection === computerSelection){
+    if (playerSelection === computerSelection) {
         return `It's a tie!`
     }
-    if(
-        (playerSelection === 'rock' && computerSelection === 'scissors') 
+    if (
+        (playerSelection === 'rock' && computerSelection === 'scissors')
         || (playerSelection === 'paper' && computerSelection === 'rock')
         || (playerSelection === 'scissors' && computerSelection === 'paper')
-    ){
-            return "player wins!"
-        }
-        
-    if ((computerSelection === 'rock' && playerSelection === 'scissors')
-        ||(computerSelection === 'paper' && playerSelection === 'rock')
-        ||(computerSelection === 'scissors' && playerSelection === 'paper')
-    ){
-            return 'computer wins!'
-        }
-    
+    ) {
+        return "player wins!"
+    }
+    else {
+        return "computer wins!"
+    }
+
 }
-console.log(playRound('paper', getComputerChoice()))
 
 
-function playGame(){
-    let playerScore = ""
-    let computerScore = ""
-    let round = 0
+
+function playerChoice() {
+    let input = prompt('Type Rock, Paper, or Scissors')
+    input = input.toLowerCase()
+    return input
 }
+
+
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        const playerDecision = playerChoice()
+        const computerDecision = getComputerChoice()
+        console.log(playRound(playerDecision, computerDecision))
+    }
+}
+playGame()
+
